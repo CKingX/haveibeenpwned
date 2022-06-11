e7f43716cea13f51e04f6798fbf90898764466c0
CKingX <chirag.kahlon@gmail.com>
2022-06-11 14:24:08 -0700

    Bumped version to v1.0.0 and improved performance of file_check

--- src/file_check.rs
+++ src/file_check.rs
@@ -26,34 +26,39 @@ pub fn file_check(password_file: OsString, filter: OsString, print_passwords: bo
 
     let file = std::io::BufReader::new(file);
 
-    let total_count = AtomicI32::new(0);
+    let mut total_count = 0;
     let compromised_count = AtomicI32::new(0);
 
-    let result = file.lines().par_bridge().try_for_each(|password| {
-        if password.is_err() {
-            eprintln!("unable to read password from password file");
-            return Err(());
-        }
-        total_count.fetch_add(1, Ordering::Relaxed);
-
-        let password = password.unwrap();
-        if let Password::CompromisedPassword = filter.check_password(&password) {
-            compromised_count.fetch_add(1, Ordering::Relaxed);
-            if print_passwords {
-                println!("{password}");
+    let result = file
+        .lines()
+        .map(|i| {
+            total_count += 1;
+            i
+        })
+        .par_bridge()
+        .try_for_each(|password| {
+            if password.is_err() {
+                eprintln!("unable to read password from password file");
+                return Err(());
             }
-        }
 
-        Ok(())
-    });
+            let password = password.unwrap();
+            if let Password::CompromisedPassword = filter.check_password(&password) {
+                compromised_count.fetch_add(1, Ordering::Relaxed);
+                if print_passwords {
+                    println!("{password}");
+                }
+            }
+
+            Ok(())
+        });
 
     if result.is_err() {
         return;
     }
 
     println!(
-        "Out of {}, there were {} compromised passwords",
-        total_count.load(Ordering::Relaxed),
+        "Out of {total_count}, there were {} compromised passwords",
         compromised_count.load(Ordering::Relaxed)
     );
 }
